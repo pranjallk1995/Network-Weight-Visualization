a14126a08187f53d27e73dcc3b95e1635468e950
Pranjall Kumar <pranjallk1995@gmail.com>
2025-04-02 09:04:18 +0530

    UPDATE: adding required placeholdders

--- app/main.py
+++ app/main.py
@@ -1,4 +1,18 @@
 """app entry point"""
 
+class DemoNetwork():
+    """class to create a showcase network"""
+
+    def __init__(self):
+        pass
+
+
+class VizNetwork():
+    """class to implement the weight vizualization"""
+
+    def __init__(self):
+        pass
+
+
 if __name__ == "__main__":
     pass
